ae75dfaed9ac1cefc3cadbd49ee15e5d5341c064
Vedansh Jain <vedansh.code@gmail.com>
2024-10-07 22:43:58 +0530

    Button added

--- script.js
+++ script.js
@@ -1,2 +1,8 @@
 console.log("JS file")
 console.log("hellooooooooooooo")
+
+let btn = document.getElementsByClassName("btn-count");
+
+btn.addEventListener("click", function(){
+    alert("Button clicked")
+})
