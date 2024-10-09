382d8a83d5c5a84b9539f6444b6d2d81a7563fa7
mokshag27 <mokshagawada@gmail.com>
2024-10-09 22:50:25 +0530

    added changes to all 3 files

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 console.log("JS file")
 console.log("hellooooooooooooo")
-
-let btn = document.getElementsByClassName("btn-count");
+let count = 0;
+let btn = document.getElementsByClassName("btn-count")[0];
+const countDisplay = document.getElementById("count");
 
 btn.addEventListener("click", function(){
+    count++;
+    countDisplay.textContent = "Count: " + count;
     alert("Button clicked")
 })
